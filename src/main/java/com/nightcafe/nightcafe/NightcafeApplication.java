bf1822504493a94c6e7ac2844d3388737394ddca
Muhammet Tekin <muhammettekin20de@gmail.com>
2023-07-17 09:29:20 +0200

    initial commit

---
+++ src/main/java/com/nightcafe/nightcafe/NightcafeApplication.java
@@ -0,0 +1,14 @@
+package com.nightcafe.nightcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightcafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightcafeApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/nightcafe/nightcafe/NightcafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nightcafe.nightcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightcafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
